c171e0eab497fa67de5b3e0bfab0b765bf52d9f4
yangrui <yangrui@lanjingren.com>
2018-02-28 18:19:25 +0800

    init

---
+++ src/test.js
@@ -0,0 +1,15 @@
+var ElTetris= require('./eltetris'); 
+eltetris = new ElTetris(10, 20);
+var counter = 1;
+while(true) {
+  var last = eltetris.play();
+  counter ++;
+  if (last.game_over) {
+    break;
+  }
+
+//   if (counter == 5) {
+//     break;
+//   }
+}
+console.log(eltetris.rows_completed);
