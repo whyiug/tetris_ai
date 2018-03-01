b5ec0bfa9667bff5aa4e8ebb9880aa206d9b0475
yangrui <yangrui@lanjingren.com>
2018-03-01 22:22:58 +0800

    afd

--- src/eltetris.js
+++ src/eltetris.js
@@ -31,32 +31,38 @@ var f = require('./features');
 var PIECES = require('./pieces');
 
 function ElTetris(number_of_columns, number_of_rows, board) {
-    this.number_of_rows = number_of_rows;
-    this.number_of_columns = number_of_columns;
-    this.rows_completed = 0;
+  this.number_of_rows = number_of_rows;
+  this.number_of_columns = number_of_columns;
+  this.rows_completed = 0;
 
-    // The board is represented as an array of integers, one integer for each row.
-    this.board = board;
+  // The board is represented as an array of integers, one integer for each row.
+//  this.board = board;
+  this.board = new Array(number_of_rows);
+  for (var i = 0; i < number_of_rows; i++) {
+    this.board[i] = 0;
+  }
 
-    this.FULLROW = Math.pow(2, number_of_columns) - 1;
+  this.FULLROW = Math.pow(2, number_of_columns) - 1;
 }
 
 ElTetris.prototype.play = function(piece) {
-    piece = PIECES[piece];
-    //console.log(piece);
-    var move = this.pickMove(piece);
+  var piece = this.getRandomPiece();
 
-    var last_move = this.playMove(this.board, move.orientation, move.column);
+  // piece = PIECES[piece];
+  //console.log(piece);
+  var move = this.pickMove(piece);
 
-    if (!last_move.game_over) {
-        this.rows_completed += last_move.rows_removed;
-    }
-    //  console.log(this.board);
-    //  console.log(piece);
-    //  console.log(this.rows_completed);
-    //  console.log(move);
-    return move;
-    // return last_move;
+  var last_move = this.playMove(this.board, move.orientation, move.column);
+
+  if (!last_move.game_over) {
+    this.rows_completed += last_move.rows_removed;
+  }
+  //  console.log(this.board);
+  //  console.log(piece);
+  //  console.log(this.rows_completed);
+  //  console.log(move);
+  // return move;
+  return last_move;
 };
 
 /**
@@ -76,39 +82,39 @@ ElTetris.prototype.play = function(piece) {
  */
 
 ElTetris.prototype.pickMove = function(piece) {
-    var best_evaluation = -100000;
-    var best_orientation = 0;
-    var best_column = 0;
-    var evaluation = undefined;
+  var best_evaluation = -100000;
+  var best_orientation = 0;
+  var best_column = 0;
+  var evaluation = undefined;
 
-    // Evaluate all possible orientations
-    for (var i in piece) {
-        var orientation = piece[i].orientation;
+  // Evaluate all possible orientations
+  for (var i in piece) {
+    var orientation = piece[i].orientation;
 
-        // Evaluate all possible columns
-        for (var j = 0; j < this.number_of_columns - piece[i].width + 1; j++) {
-            // Copy current board
-            var board = this.board.slice();
-            var last_move = this.playMove(board, orientation, j);
+    // Evaluate all possible columns
+    for (var j = 0; j < this.number_of_columns - piece[i].width + 1; j++) {
+      // Copy current board
+      var board = this.board.slice();
+      var last_move = this.playMove(board, orientation, j);
 
-            if (!last_move.game_over) {
-                evaluation = this.evaluateBoard_EL(last_move, board);
+      if (!last_move.game_over) {
+        evaluation = this.evaluateBoard_EL(last_move, board);
 
-                if (evaluation > best_evaluation) {
-                    best_evaluation = evaluation;
-                    best_orientation = i;
-                    best_column = j;
-                }
-            }
+        if (evaluation > best_evaluation) {
+          best_evaluation = evaluation;
+          best_orientation = i;
+          best_column = j;
         }
+      }
     }
+  }
 
-    // console.log(piece[best_orientation]);
-    return {
-        'orientation': piece[best_orientation].orientation,
-        'index': piece[best_orientation].index,
-        'column': best_column,
-    };
+  // console.log(piece[best_orientation]);
+  return {
+    'orientation': piece[best_orientation].orientation,
+    'index': piece[best_orientation].index,
+    'column': best_column,
+  };
 };
 
 /**
@@ -126,20 +132,20 @@ ElTetris.prototype.pickMove = function(piece) {
  *   better the board.
  */
 ElTetris.prototype.evaluateBoard2_PD = function(last_move, board) {
-    return GetLandingHeight(last_move, board) * -1 +
-        last_move.rows_removed * 1 +
-        GetRowTransitions(board, this.number_of_columns) * -1 +
-        GetColumnTransitions(board, this.number_of_columns) * -1 +
-        GetNumberOfHoles(board, this.number_of_columns) * -1 +
-        GetWellSums(board, this.number_of_columns) * -1;
+  return f.GetLandingHeight(last_move, board) * -1 +
+    last_move.rows_removed * 1 +
+    f.GetRowTransitions(board, this.number_of_columns) * -1 +
+    f.GetColumnTransitions(board, this.number_of_columns) * -1 +
+    f.GetNumberOfHoles(board, this.number_of_columns) * -1 +
+    f.GetWellSums(board, this.number_of_columns) * -1;
 };
 ElTetris.prototype.evaluateBoard_EL = function(last_move, board) {
-    return GetLandingHeight(last_move, board) * -4.500158825082766 +
-        last_move.rows_removed * 3.4181268101392694 +
-        GetRowTransitions(board, this.number_of_columns) * -3.2178882868487753 +
-        GetColumnTransitions(board, this.number_of_columns) * -9.348695305445199 +
-        GetNumberOfHoles(board, this.number_of_columns) * -7.899265427351652 +
-        GetWellSums(board, this.number_of_columns) * -3.3855972247263626;
+  return f.GetLandingHeight(last_move, board) * -4.500158825082766 +
+    last_move.rows_removed * 3.4181268101392694 +
+    f.GetRowTransitions(board, this.number_of_columns) * -3.2178882868487753 +
+    f.GetColumnTransitions(board, this.number_of_columns) * -9.348695305445199 +
+    f.GetNumberOfHoles(board, this.number_of_columns) * -7.899265427351652 +
+    f.GetWellSums(board, this.number_of_columns) * -3.3855972247263626;
 };
 /**
  * Play the given piece at the specified location.
@@ -153,76 +159,76 @@ ElTetris.prototype.evaluateBoard_EL = function(last_move, board) {
  *   True if play succeeded, False if game is over.
  */
 ElTetris.prototype.playMove = function(board, piece, column) {
-    piece = this.movePiece(piece, column);
-    var placementRow = this.getPlacementRow(board, piece);
-    var rowsRemoved = 0;
+  piece = this.movePiece(piece, column);
+  var placementRow = this.getPlacementRow(board, piece);
+  var rowsRemoved = 0;
 
-    if (placementRow + piece.length > this.number_of_rows) {
-        // Game over.
-        return { 'game_over': true };
+  if (placementRow + piece.length > this.number_of_rows) {
+    // Game over.
+    return { 'game_over': true };
+  }
+
+  // Add piece to board.
+  for (var i = 0; i < piece.length; i++) {
+    board[placementRow + i] |= piece[i];
+  }
+
+  // Remove any full rows
+  for (i = 0; i < piece.length; i++) {
+    if (board[placementRow + i] == this.FULLROW) {
+      board.splice(placementRow + i, 1);
+      // Add an empty row on top.
+      board.push(0);
+      // Since we have decreased the number of rows by one, we need to adjust
+      // the index accordingly.
+      i--;
+      rowsRemoved++;
     }
+  }
 
-    // Add piece to board.
-    for (var i = 0; i < piece.length; i++) {
-        board[placementRow + i] |= piece[i];
-    }
-
-    // Remove any full rows
-    for (i = 0; i < piece.length; i++) {
-        if (board[placementRow + i] == this.FULLROW) {
-            board.splice(placementRow + i, 1);
-            // Add an empty row on top.
-            board.push(0);
-            // Since we have decreased the number of rows by one, we need to adjust
-            // the index accordingly.
-            i--;
-            rowsRemoved++;
-        }
-    }
-
-    return {
-        'landing_height': placementRow,
-        'piece': piece,
-        'rows_removed': rowsRemoved,
-        'game_over': false
-    };
+  return {
+    'landing_height': placementRow,
+    'piece': piece,
+    'rows_removed': rowsRemoved,
+    'game_over': false
+  };
 };
 
 /**
  * Given a piece, return the row at which it should be placed.
  */
 ElTetris.prototype.getPlacementRow = function(board, piece) {
-    // Descend from top to find the highest row that will collide
-    // with the our piece.
-    for (var row = this.number_of_rows - piece.length; row >= 0; row--) {
-        // Check if piece collides with the cells of the current row.
-        for (var i = 0; i < piece.length; i++) {
-            if ((board[row + i] & piece[i]) !== 0) {
-                // Found collision - place piece on row above.
-                return row + 1;
-            }
-        }
+  // Descend from top to find the highest row that will collide
+  // with the our piece.
+  for (var row = this.number_of_rows - piece.length; row >= 0; row--) {
+    // Check if piece collides with the cells of the current row.
+    for (var i = 0; i < piece.length; i++) {
+      if ((board[row + i] & piece[i]) !== 0) {
+        // Found collision - place piece on row above.
+        return row + 1;
+      }
     }
+  }
 
-    return 0; // No collision found, piece should be placed on first row.
+  return 0; // No collision found, piece should be placed on first row.
 };
 
 ElTetris.prototype.movePiece = function(piece, column) {
-    // Make a new copy of the piece
-    var newPiece = piece.slice();
-    for (var i = 0; i < piece.length; i++) {
-        newPiece[i] = piece[i] << column;
-    }
+  // Make a new copy of the piece
+  var newPiece = piece.slice();
+  for (var i = 0; i < piece.length; i++) {
+    newPiece[i] = piece[i] << column;
+  }
 
-    return newPiece;
+  return newPiece;
 };
 
 ElTetris.prototype.getRandomPieceIndex = function() {
-    return Math.floor(Math.random() * PIECES.length);
+  return Math.floor(Math.random() * PIECES.length);
 };
 
 ElTetris.prototype.getRandomPiece = function() {
-    return PIECES[this.getRandomPieceIndex()];
+  return PIECES[this.getRandomPieceIndex()];
 };
 
 module.exports = ElTetris;
---
+++ src/test_ai.js
@@ -0,0 +1,53 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({     
+  host     : 'localhost',       
+  user     : 'root',              
+  password : '031313',       
+  port: '3306',                   
+  database: 'tetris', 
+}); 
+connection.connect();
+var ElTetris= require('./eltetris'); 
+eltetris = new ElTetris(10, 20);
+var row0 = 0;
+var row1 = 0;
+var row2 = 0;
+var row3 = 0;
+var row4 = 0;
+var score = 0;
+var counter = 0;
+while(true) {
+  var last = eltetris.play();
+  if (counter == 20000) {
+    break;
+  }
+  counter ++;
+  if (last.game_over) {
+    break;
+  }
+  switch (last.rows_removed) {
+    case 0: 
+      row0 ++;
+      break;
+    case 1: 
+      row1 ++;
+      break;
+    case 2: 
+      row2 ++;
+      break;
+    case 3: 
+      row3 ++;
+      break;
+    case 4: 
+      row4 ++;
+      break;
+  }
+  score = eltetris.rows_completed;
+
+  var sql = "insert into pd_ai_1 (row0, row1, row2, row3, row4, score, counters)values(?, ?, ?, ?, ?, ?, ?)";
+  var params = [row0, row1, row2, row3, row4, score, counter];
+  connection.query(sql, params);
+  console.log(eltetris.rows_completed);
+}
+connection.end();
+console.log(eltetris.rows_completed);
