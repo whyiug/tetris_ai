74dd26ca1736e0d4a12cea68c6941f669d3fb75e
yangrui <yangrui@lanjingren.com>
2018-03-02 18:03:32 +0800

    asdf

--- src/client.js
+++ src/client.js
@@ -1,12 +1,12 @@
 var net = require('net'); //引入网络模块
-// var HOST = '192.168.0.141'; //定义服务器地址
-var HOST = '127.0.0.1'; //定义服务器地址
+var HOST = '192.168.0.141'; //定义服务器地址
+// var HOST = '127.0.0.1'; //定义服务器地址
 var PORT = 12345; //定义端口号
 
 //创建一个TCP客户端实例
 var client = net.connect(PORT, HOST, function() {
     console.log('Connected to the server.');
-    client.write('el2\r\n');
+    client.write('tourist\r\n');
 });
 
 var rows = 0;
